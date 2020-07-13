11688b2a2ba59f590644a1eaedfbda7384930c37
jimmeyCheung <100178981@qq.com>
2020-07-13 22:15:17 +0800

    重构row和col

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,6 @@
 <template>
-  <div
-    class="col"
-    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}"
-  >
-    <div style="border: 1px solid green; height: 100px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 <script>
@@ -20,16 +14,22 @@ export default {
       type: [Number, String]
     }
   },
+  computed: {
+    colClass() {
+      let { span, offset } = this;
+      return [span && `col-${span}`, offset && `offset-${offset}`];
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px"
+      };
+    }
+  },
   data() {
     return {
       gutter: 0
     };
-  },
-  created() {
-    console.log("col created");
-  },
-  mounted() {
-    console.log("col mounted");
   }
 };
 </script>
