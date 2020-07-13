5699e4bac54aa762112afd0749bf8574193da67c
JimmeyCheung <100178981@qq.com>
2020-07-13 10:26:58 +0800

    基本完成g-row和g-col组件

--- src/app.js
+++ src/app.js
@@ -3,27 +3,29 @@ import Button from "./button";
 import Icon from "./icon";
 import ButtonGroup from "./button-group";
 import Input from './input';
+import Row from './row';
+import Col from './col';
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
 Vue.component("g-button-group", ButtonGroup);
 Vue.component('g-input', Input);
-
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
 new Vue({
-  el: "#app",
-  data: () => {
-    return {
-      loading1: false,
-      loading2: false,
-      loading3: false,
-      message: 'hi'
-    };
-  },
-  created() {
-  },
-  methods: {
-    inputChange(e) {
-      console.log(e)
+    el: "#app",
+    data: () => {
+        return {
+            loading1: false,
+            loading2: false,
+            loading3: false,
+            message: 'hi'
+        };
+    },
+    created() {},
+    methods: {
+        inputChange(e) {
+            console.log(e)
+        }
     }
-  }
 });
