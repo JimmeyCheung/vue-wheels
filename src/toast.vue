8d5c5df9e60a2adc75a2968abf07e7636c3c1847
jimmeyCheung <100178981@qq.com>
2020-07-14 22:45:13 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -89,7 +89,18 @@ export default {
 $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
 .toast {
+  animation: fade-in 1s;
   font-size: $font-size;
   min-height: $toast-min-height;
   line-height: 1.8;
