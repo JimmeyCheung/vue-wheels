b97e6ed5d03f9e35ed190cac7f2e69145d21b6ba
jimmeyCheung <100178981@qq.com>
2020-07-15 22:57:16 +0800

    完善tab样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -23,11 +23,13 @@ export default {
 <style scoped lang="scss">
 $tab-height: 40px;
 $blue: blue;
+$border-color: #ddd;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
   position: relative;
+  border-bottom: 1px solid $border-color;
   > .line {
     position: absolute;
     bottom: 0;
@@ -37,6 +39,10 @@ $blue: blue;
   }
   > .actions-wrapper {
     margin-left: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
   }
 }
 </style> 
