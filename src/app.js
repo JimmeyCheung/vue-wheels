758429c172a71e8618ae3cf18104415baf97b638
jimmeyCheung <100178981@qq.com>
2020-07-20 21:13:33 +0800

    确定如何渲染内容

--- src/app.js
+++ src/app.js
@@ -5,49 +5,49 @@ import ButtonGroup from "./button-group";
 import Input from './input';
 import Row from './row';
 import Col from './col';
-import Layout from './layout'
-import Header from './header'
-import Sider from './sider'
-import Content from './content'
-import Footer from './footer'
-import Toast from './toast'
-import plugin from './plugin'
-import Tabs from './tabs'
-import TabsHead from './tabs-head'
-import TabsBody from './tabs-body'
-import TabsItem from './tabs-item'
-import TabsPane from './tabs-pane'
-import Popover from './popover'
-import Collapse from './collapse'
-import CollapseItem from './collapse-item'
-import Cascader from './cascader'
+import Layout from './layout';
+import Header from './header';
+import Sider from './sider';
+import Content from './content';
+import Footer from './footer';
+import Toast from './toast';
+import plugin from './plugin';
+import Tabs from './tabs';
+import TabsHead from './tabs-head';
+import TabsBody from './tabs-body';
+import TabsItem from './tabs-item';
+import TabsPane from './tabs-pane';
+import Popover from './popover';
+import Collapse from './collapse';
+import CollapseItem from './collapse-item';
+import Cascader from './cascader';
 
 Vue.component("g-button", Button);
 Vue.component("g-button-group", ButtonGroup);
-Vue.component('g-cascader', Cascader)
+Vue.component('g-cascader', Cascader);
 Vue.component('g-col', Col);
-Vue.component('g-collapse', Collapse)
-Vue.component('g-collapse-item', CollapseItem)
-Vue.component('g-content', Content)
-Vue.component('g-footer', Footer)
-Vue.component('g-header', Header)
-Vue.component('g-icon', Icon)
-Vue.component('g-input', Input)
-Vue.component('g-layout', Layout)
-Vue.component('g-popover', Popover)
-Vue.component('g-row', Row)
-Vue.component('g-sider', Sider)
-Vue.component('g-tabs', Tabs)
-Vue.component('g-tabs-body', TabsBody)
-Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-item', TabsItem)
-Vue.component('g-tabs-pane', TabsPane)
-Vue.component('g-toast', Toast)
-Vue.use(plugin)
+Vue.component('g-collapse', Collapse);
+Vue.component('g-collapse-item', CollapseItem);
+Vue.component('g-content', Content);
+Vue.component('g-footer', Footer);
+Vue.component('g-header', Header);
+Vue.component('g-icon', Icon);
+Vue.component('g-input', Input);
+Vue.component('g-layout', Layout);
+Vue.component('g-popover', Popover);
+Vue.component('g-row', Row);
+Vue.component('g-sider', Sider);
+Vue.component('g-tabs', Tabs);
+Vue.component('g-tabs-body', TabsBody);
+Vue.component('g-tabs-head', TabsHead);
+Vue.component('g-tabs-item', TabsItem);
+Vue.component('g-tabs-pane', TabsPane);
+Vue.component('g-toast', Toast);
+Vue.use(plugin);
 
-import createElement from 'vue'
+import createElement from 'vue';
 
-const h = createElement
+const h = createElement;
 
 
 new Vue({
@@ -86,6 +86,16 @@ new Vue({
                         ]
                     },
                 ]
+            }, {
+                name: '安徽',
+                children: [{
+                    name: '合肥',
+                    children: [{
+                        name: '瑶海'
+                    }, {
+                        name: '庐阳'
+                    }]
+                }]
             }]
         };
     },
