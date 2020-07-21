66bee7be8a1a4b4c820a2e9dbb0ed78597392a5a
jimmeyCheung <100178981@qq.com>
2020-07-21 21:31:58 +0800

    优化cascader组件样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cascader">
-    <div class="trigger" @click="popoverVisible = !popoverVisible"></div>
+    <div class="trigger" @click="popoverVisible = !popoverVisible">{{result || '&nbsp;'}}</div>
     <div class="popover-wrapper" v-if="popoverVisible">
       <cascader-items
         :items="source"
@@ -41,6 +41,11 @@ export default {
     onUpdateSelected(newSelected) {
       this.$emit("update:selected", newSelected);
     }
+  },
+  computed: {
+    result() {
+      return this.selected.map(item => item.name).join("/");
+    }
   }
 };
 </script>
@@ -50,9 +55,13 @@ export default {
 .cascader {
   position: relative;
   .trigger {
-    border: 1px solid black;
-    height: 32px;
-    width: 100px;
+    height: $input-height;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 1em;
+    min-width: 10em;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
   }
   .popover-wrapper {
     position: absolute;
@@ -60,6 +69,7 @@ export default {
     left: 0;
     background: white;
     display: flex;
+    margin-top: 8px;
     @extend .box-shadow;
   }
 }
