a513255d3d8090fb196693d0a6256c22c8117f4c
jimmeyCheung <100178981@qq.com>
2020-07-21 21:48:50 +0800

    添加测试数据，解决bug

--- src/demo.vue
+++ src/demo.vue
@@ -4,8 +4,16 @@
     <p>{{selected &&selected[1] &&selected[1].name || '空'}}</p>
     <p>{{selected &&selected[2] &&selected[2].name || '空'}}</p>
     <div style="padding: 20px;">
-      <g-cascader :source="source" popover-height="200px" :selected.sync="selected"></g-cascader>
+      <g-cascader
+        :source.sync="source"
+        popover-height="200px"
+        @update:source="onUpdateSource"
+        @update:selected="onUpdateSelected"
+        :selected.sync="selected"
+        :load-data="loadData"
+      ></g-cascader>
     </div>
+    {{source}}
   </div>
 </template>
 <script>
@@ -13,9 +21,13 @@ import Button from "./button";
 import Cascader from "./cascader";
 import db from "./db";
 function ajax(parentId = 0) {
-  return db.filter(item => item.parent_id == parentId);
+  return new Promise((success, fail) => {
+    setTimeout(() => {
+      let result = db.filter(item => item.parent_id == parentId);
+      success(result);
+    }, 300);
+  });
 }
-console.log(ajax());
 export default {
   name: "demo",
   components: {
@@ -25,8 +37,30 @@ export default {
   data() {
     return {
       selected: [],
-      source: ajax()
+      source: []
     };
+  },
+  created() {
+    ajax(0).then(result => {
+      this.source = result;
+    });
+  },
+  methods: {
+    loadData({ id }, updateSource) {
+      ajax(id).then(result => {
+        updateSource(result); // 回调:把别人传给我的函数调用一下
+      });
+    },
+    xxx() {
+      ajax(this.selected[0].id).then(result => {
+        let lastLevelSelected = this.source.filter(
+          item => item.id === this.selected[0].id
+        )[0];
+        this.$set(lastLevelSelected, "children", result);
+      });
+    },
+    onUpdateSource() {},
+    onUpdateSelected() {}
   }
 };
 </script>
