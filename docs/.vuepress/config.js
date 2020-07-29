796fa7689ee825b65ef1729e76c861da724a9435
JimmeyCheung <100178981@qq.com>
2020-07-29 13:24:49 +0800

    添加轮播组件文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,39 +4,40 @@ module.exports = {
     description: '一个好用的UI框架',
     themeConfig: {
         nav: [{
-            text: '主页',
-            link: '/'
-        },
-        {
-            text: '文档',
-            link: '/install/'
-        },
-        {
-            text: '交流',
-            link: 'https://google.com'
-        },
+                text: '主页',
+                link: '/'
+            },
+            {
+                text: '文档',
+                link: '/install/'
+            },
+            {
+                text: '交流',
+                link: 'https://google.com'
+            },
         ],
         sidebar: [{
-            title: '入门',
-            collapsable: false,
-            children: [
-                '/install/',
-                '/get-started/',
-            ]
-        },
-        {
-            title: '组件',
-            collapsable: false,
-            children: [
-                '/components/button',
-                '/components/tabs',
-                '/components/input',
-                '/components/grid',
-                '/components/layout',
-                '/components/toast',
-                '/components/popover',
-            ]
-        }
+                title: '入门',
+                collapsable: false,
+                children: [
+                    '/install/',
+                    '/get-started/',
+                ]
+            },
+            {
+                title: '组件',
+                collapsable: false,
+                children: [
+                    '/components/button',
+                    '/components/tabs',
+                    '/components/input',
+                    '/components/grid',
+                    '/components/layout',
+                    '/components/toast',
+                    '/components/popover',
+                    '/components/slides',
+                ]
+            }
 
         ]
     }
---
+++ docs/components/slides.md
@@ -0,0 +1,9 @@
+---
+title: Slides
+---
+
+# 轮播
+
+<ClientOnly>
+  <slides-demo-1></slides-demo-1>
+</ClientOnly>
